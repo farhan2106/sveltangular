14d886a2daebeab9e13e93429fd64ff5e7c0f5ac
Farhan Ghazali <farhan_ghazali@yahoo.com>
2019-11-05 07:50:56 +0800

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -4,32 +4,67 @@ const autoPreprocess = require('svelte-preprocess');
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-  // All imported modules in your tests should be mocked automatically
-  // automock: false,
-
-  // Stop running tests after `n` failures
-  // bail: 0,
-
-  // Respect "browser" field in package.json when resolving modules
-  // browser: false,
-
-  // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "/tmp/jest_rs",
-
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true, // Disabling first to figure out: "Support for the experimental syntax 'dynamicImport' isn't currently enabled (280:20):"
 
+  // A list of paths to directories that Jest should use to search for files in
+  roots: [
+    "<rootDir>"
+  ],
+
+  // The root directory that Jest should scan for tests and modules within
+  rootDir: 'src',
+
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
-    "src/**/**.{js, ts, svelte}",
-    "src/**/**.compiled.svelte"
+    "**/**.{js, ts, svelte}",
+    "**/**.compiled.svelte"
   ],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: "../coverage",
+
+  // A path to a module which exports an async function that is triggered once before all test suites
+  globalSetup: './../test-helper/setup',
+
+  // A path to a module which exports an async function that is triggered once after all test suites
+  globalTeardown: './../test-helper/teardown',
+
+  // An array of file extensions your modules use
+  moduleFileExtensions: [
+    "js",
+    "json",
+    "jsx",
+    "ts",
+    "tsx",
+    "node",
+    "svelte"
+  ],
+
+  // A map from regular expressions to paths to transformers
+  "transform": {
+    "\\.js$": "babel-jest",
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.svelte$': 'jest-transform-svelte'
+  },
+
+  // Indicates whether each individual test should be reported during the run
+  verbose: true,
+
+  // Respect "browser" field in package.json when resolving modules
+  browser: true,
+
+  // All imported modules in your tests should be mocked automatically
+  // automock: false,
+
+  // Stop running tests after `n` failures
+  // bail: 0,
+
+  // The directory where Jest should store its cached dependency information
+  // cacheDirectory: "/tmp/jest_rs",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -56,12 +91,6 @@ module.exports = {
   // Force coverage collection from ignored files using an array of glob patterns
   // forceCoverageMatch: [],
 
-  // A path to a module which exports an async function that is triggered once before all test suites
-  globalSetup: './test-helper/setup',
-
-  // A path to a module which exports an async function that is triggered once after all test suites
-  globalTeardown: './test-helper/teardown',
-
   // A set of global variables that need to be available in all test environments
   // globals: {},
 
@@ -72,17 +101,6 @@ module.exports = {
   // moduleDirectories: [
   //   "node_modules"
   // ],
-
-  // An array of file extensions your modules use
-  moduleFileExtensions: [
-    "js",
-    "json",
-    "jsx",
-    "ts",
-    "tsx",
-    "node",
-    "svelte"
-  ],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
@@ -116,14 +134,6 @@ module.exports = {
 
   // Automatically restore mock state between every test
   // restoreMocks: false,
-
-  // The root directory that Jest should scan for tests and modules within
-  // rootDir: null,
-
-  // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
@@ -172,13 +182,6 @@ module.exports = {
   // Setting this value to "fake" allows the use of fake timers for functions such as "setTimeout"
   // timers: "real",
 
-  // A map from regular expressions to paths to transformers
-  "transform": {
-    "\\.js$": "babel-jest",
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.svelte$': 'jest-transform-svelte'
-  },
-
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
   //   "/node_modules/"
@@ -186,9 +189,6 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
-
-  // Indicates whether each individual test should be reported during the run
-  verbose: true,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
