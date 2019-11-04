ffda6e8d4ada5dda5c085019c79ac1bff931197d
Farhan Ghazali <farhan_ghazali@yahoo.com>
2019-11-04 17:15:25 +0800

    Update

--- jest.config.js
+++ jest.config.js
@@ -174,7 +174,8 @@ module.exports = {
   // A map from regular expressions to paths to transformers
   "transform": {
     "\\.js$": "babel-jest",
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.svelte$': ['jest-transform-svelte'/*, { preprocess: sveltePreprocess() }*/]
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
