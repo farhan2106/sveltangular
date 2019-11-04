ca0442057b1576edfae0ff7f249543b3eb808a70
Farhan Ghazali <farhan_ghazali@yahoo.com>
2019-11-04 15:28:48 +0800

    Initial

--- src/pages/Sample.test.js
+++ src/pages/Sample.test.js
@@ -1,6 +1,8 @@
-import Sample from './Sample.svelte';
+import path from 'path'
+import testHelper from './../../test-helper';
 
-test('component should load', () => {
+test('component should load', async () => {
+  const Sample = await testHelper(path.resolve(__dirname, 'Sample.svelte'))
   new Sample({
     target: document.body
   })
--- src/services/Sample.ts
+++
@@ -1,3 +0,0 @@
-export default () => ({
-  add: (x: number, y: number) => (x + y)
-})
